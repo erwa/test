47102c21bf8ce285530c6f8c18e489ac5b9807d4
Anthony Hsu <erwaman@gmail.com>
2014-05-31 14:15:14 -0700

    Added .gitignore, Test.java, jquery, jquery.editinplace, test.html

---
+++ Test.java
@@ -0,0 +1,12 @@
+class Test<T>{
+    public T[] gen1() {
+        T[] ans = (T[]) new Object[3];
+        return ans;
+    }
+
+     public static void main(String[] args) {
+     	String[] strs = new String[] {"abc","aaa","a","aba","wftwtfw","",null,"AA","lillil"};
+
+     	
+     }
+}
