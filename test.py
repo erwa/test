903cec7812ae5efe6b5926207672b74f804d4878
Anthony Hsu <erwaman@gmail.com>
2014-06-18 17:15:20 -0700

    Added test.groovy, test.py, modified test.sh for string matching/splicing testing

---
+++ test.py
@@ -0,0 +1,15 @@
+import os
+
+def a():
+  return 1,2
+
+# for root, dirs, files in os.walk('.'):
+  # print root, dirs, files
+c,b = a()
+print c,b
+# for f in files:
+  # print f, " is ", os.path.isfile(f)
+# l = ['a']
+# print l
+
+# print "%s,%s" % ("a", "b")
--- test.sh
+++ test.sh
@@ -1,1 +1,8 @@
-echo $0
+if [[ $1 != *.tar.gz ]]
+then
+  echo "not .tar.gz"
+else
+  echo "is .tar.gz"
+
+  echo ${1%.tar.gz}
+fi
